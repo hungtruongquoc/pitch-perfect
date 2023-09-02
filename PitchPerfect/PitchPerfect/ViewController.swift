7e45aa75535e8d6e273e97440ce6f1e02c552c14
Hung Truong <hungtruongquoc@gmail.com>
2023-09-01 22:44:05 -0500

    20230901 - Added initial code

---
+++ PitchPerfect/PitchPerfect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Hung Truong on 9/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
