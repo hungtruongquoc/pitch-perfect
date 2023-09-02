b3dba553d89b62d0d5634aa8dd33785365c2e446
Hung Truong <hungtruongquoc@gmail.com>
2023-09-02 18:35:57 -0500

    20230902: Added new buttons to the UI

--- PitchPerfect/PitchPerfect/ViewController.swift
+++ PitchPerfect/PitchPerfect/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblRecording: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func recordAudio(_ sender: Any) {
+        print("Record button was pressed")
+        lblRecording.text = "Recording in Progress"
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        print("Stop Recording button was pressed")
+    }
 }
 
